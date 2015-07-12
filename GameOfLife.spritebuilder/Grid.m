2ac10ccb2590069038844544bfb2dedf92112db8
flyingpie101 <flyingpie101@users.noreply.github.com>
2015-07-11 21:26:00 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -157,9 +157,9 @@ static const int GRID_COLUMNS = 10;
 
 -(void) updateCreatures
 {
-    for (int i=0; i < GRID_ROWS; i++)
+    for (int i=0; i < [_gridArray count]; i++)
     {
-        for (int j=0; j < GRID_COLUMNS; j++)
+        for (int j=0; j < [_gridArray[i] count]; j++)
         {
             Creature *currentCreature = _gridArray[i][j];
             if (currentCreature.livingNeighbors == 3)
